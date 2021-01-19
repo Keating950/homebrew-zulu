1f7e237f7f81da948f8644ae752c99fd0a55d15a
Keating Reid <keating.reid@protonmail.com>
2021-01-19 09:34:43 -0500

    updating jdk15 formula

--- Casks/zulu-jdk15.rb
+++ Casks/zulu-jdk15.rb
@@ -1,13 +1,13 @@
 cask 'zulu-jdk15' do
-  version '15.28.51,15.0.1'
-  sha256 '991311eaa854b2c6ec7747d548d3208099b293ef2eb3727320ba83c875154777'
+  version '15.28.1013,15.0.1'
+  sha256 '055A493236ED0023216F40D4E222EBC9CEBD13BCAA8288D9F6C6C5C2D61F30EE'
 
-  url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_x64.dmg",
+  url "https://cdn.azul.com/zulu/bin/zulu#{version.before_comma}-ca-jdk#{version.after_comma}-macosx_aarch64.dmg",
       referer: 'https://www.azul.com/downloads/zulu-community/'
   name 'Azul Zulu® JDK 15'
   homepage 'https://www.azul.com/downloads/zulu-community/'
 
-  depends_on macos: '>= :high_sierra'
+  depends_on macos: '>= :big_sur'
 
   pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
