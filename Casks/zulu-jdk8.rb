4eeb5559e69b74c50051305b244be6f37e868d22
Keating Reid <keating.reid@protonmail.com>
2021-01-19 09:44:17 -0500

    updating jdk8

--- Casks/zulu-jdk8.rb
+++ Casks/zulu-jdk8.rb
@@ -1,13 +1,12 @@
 cask 'zulu-jdk8' do
-    version '8.0.275,8.50.0.51'
-    sha256 'a1a50b751a545da211c6e5546a45518202a4a558e0810341e2f4089eecbb16f4'
-
-    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-ca-jdk#{version.before_comma}-macosx_x64.dmg",
+  version '8.50.0.1017,8.0.275'
+    sha256 '80F1E48D017896E05B5722A3DE19F799E5CB9854EC5A1883D0E7A9A9ED4E7B23'
+    url "https://cdn.azul.com/zulu/bin/zulu#{version.after_comma}-ca-jdk#{version.before_comma}-macosx_aarch64.dmg",
         referer: 'https://www.azul.com/downloads/zulu-community/'
     name 'Azul Zulu® JDK 8'
     homepage 'https://www.azul.com/downloads/zulu-community/'
 
-    depends_on macos: '>= :high_sierra'
+    depends_on macos: '>= :big_sur'
 
     pkg "Double-Click to Install Zulu #{version.major}.pkg"
 
